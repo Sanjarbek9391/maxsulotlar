d2ead7a7e4428242f0194a96b85cf38da5d73c5b
azamat <azamatbekxoshimov571>
2024-12-18 19:56:35 +0500

    new market

--- market.py
+++ market.py
@@ -3,11 +3,11 @@ from mahsulotlar import tavarlar
 
 
 class Market:
-    def __init__(self, title, baza, balance):
+    def __init__(self, title, balance):
         self.title = title
         self.baza = []
         self.balance = balance
 
 
-market = Market("Uzum Market", None, 10000)
+market = Market("Uzum Market",10000)
 market.baza.append(tavarlar)
--- working.py
+++ working.py
@@ -4,7 +4,19 @@ from market import market
 from mahsulotlar import tavarlar
 from history import History
 
+
 def work():
     while True:
-        print(f"{market.title}ga xush kelibsiz!")
+        status = input(f"{market.title}ga xush kelibsiz!"
+                       f"\nMahsulotlarni ko`rish: 1 \nKorzinkani ko`rish: 2 \nChiqish: 0 ")
+        if status == "0":
+            break
+        if status == "1":
+            for item in tavarlar:
+                print(f"Mahsulot nomi: {item.nomi}, narxi: {item.narx}")
+            status1 = input("Mahsulot nomini kiriting: ")
+            miqdori = int(input("Miqdorini kiriting: "))
 
+
+
+work()
