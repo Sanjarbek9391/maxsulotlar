3f7bd28747e44e9145b11d2ab0b8ae86213f48f4
Sanjarbek9391 <sanjarbekarzikulov@gmail.com>
2024-12-21 19:08:16 +0500

    Update mahsulotlar.py

--- mahsulotlar.py
+++ mahsulotlar.py
@@ -6,7 +6,7 @@ class Mahsulotlar:
         self.muddat = muddat
         self.soni = 100
 
-
+tavarlar = [
 tavar1 = Mahsulotlar("kartoshka", 9000, "02/02/2025")
 tavar2 = Mahsulotlar("fanta", 15000, "02/06/2025")
 tavar3 = Mahsulotlar("go`sht", 110000, "02/01/2025")
@@ -17,4 +17,4 @@ tavar7 = Mahsulotlar("cola", 15000, "02/09/2025")
 tavar8 = Mahsulotlar("shokalad", 30000, "02/12/2025")
 tavar9 = Mahsulotlar("guruch", 25000, "02/01/2026")
 tavar10 = Mahsulotlar("shakar", 14000, "02/03/2026")
-tavarlar = [tavar1, tavar2, tavar3, tavar4, tavar5, tavar6, tavar7, tavar8, tavar9, tavar10]
+]
